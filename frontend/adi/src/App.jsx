d0a45383afed27503ddb43c239f2de5bb21aaefb
Srishruti24 <sri.shruti24@gmail.com>
2025-10-29 10:15:53 +0530

    `Refactored HTML layout: replaced table with separate infoBox divs for language and address`

--- frontend/adi/src/App.jsx
+++ frontend/adi/src/App.jsx
@@ -1,12 +1,18 @@
-
-import './App.css'
+import Card from './components/Card.jsx';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
 
   return (
     <>
+<Navbar/>
       <div>
-        ABES COLLEGE
+       <Card name="Shruti" age={18}/>
+       <br />
+       <Card name="Meher" age={28}/>
+       <br />
+       <Card name="Vidhya" age={16}/>
+       
       </div>
     </>
   )
