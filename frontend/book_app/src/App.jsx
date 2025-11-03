8047ac479c115c94e5269caf97ba126d3c288466
Srishruti24 <sri.shruti24@gmail.com>
2025-11-03 13:07:04 +0530

    Book_app update

--- frontend/book_app/src/App.jsx
+++ frontend/book_app/src/App.jsx
@@ -1,10 +1,14 @@
-
+import React from 'react';
+import './App.css';
+import Book from './Components/Book';
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div id="container">
+      <Book/>
+      <Book/>
+      <Book/>
+    </div>
   )
 }
 
