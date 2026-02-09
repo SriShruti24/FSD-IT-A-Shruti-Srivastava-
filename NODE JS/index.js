e1cc3a58f67cbfadf642000c0cb4a3db07088c4d
Srishruti24 <sri.shruti24@gmail.com>
2026-02-09 11:48:58 +0530

    feat: add text files and update package.json for development script

--- NODE JS/index.js
+++ NODE JS/index.js
@@ -1,49 +1,55 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
 
-const myserver = http.createServer((req, res) => {
+// const myserver = http.createServer((req, res) => {
 
-    // HOME
-    if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>Welcome to my home page</h1>");
-    }
+//     // HOME
+//     if (req.url === "/") {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end("<h1>Welcome to my home page</h1>");
+//     }
 
-    // CONTACT
-    else if (req.url === "/contact") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>Contact us at: 567-890-1234</h1>");
-    }
+//     // CONTACT
+//     else if (req.url === "/contact") {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end("<h1>Contact us at: 567-890-1234</h1>");
+//     }
 
-    // ABOUT PAGE (HTML response)
-    else if (req.url === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(`
-            <h1>ABES ENGINEERING COLLEGE</h1>
-            <img src="/abes.jpg" width="400" height="300" />
-        `);
-    }
+//     // ABOUT PAGE (HTML response)
+//     else if (req.url === "/about") {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end(`
+//             <h1>ABES ENGINEERING COLLEGE</h1>
+//             <img src="/abes.jpg" width="400" height="300" />
+//         `);
+//     }
 
    
-    else if (req.url === "/abes.jpg") {
-        const imagePath = path.join(__dirname, "images", "abes.jpg");
+//     else if (req.url === "/abes.jpg") {
+//         const imagePath = path.join(__dirname, "images", "abes.jpg");
 
-        fs.readFile(imagePath, (err, data) => {
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end("Image not found");
-            } else {
-                res.writeHead(200, { "Content-Type": "image/jpeg" });
-                res.end(data);
-            }
-        });
-    }
+//         fs.readFile(imagePath, (err, data) => {
+//             if (err) {
+//                 res.writeHead(404, { "Content-Type": "text/plain" });
+//                 res.end("Image not found");
+//             } else {
+//                 res.writeHead(200, { "Content-Type": "image/jpeg" });
+//                 res.end(data);
+//             }
+//         });
+//     }
 
-    else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>404 Page Not Found</h1>");
-    }
-});
+//     else {
+//         res.writeHead(404, { "Content-Type": "text/html" });
+//         res.end("<h1>404 Page Not Found</h1>");
+//     }
+// });
 
-myserver.listen(8000, () => console.log("Server running on port 8000"));
+// myserver.listen(8000, () => console.log("Server running on port 8000"));
+const fs=require('fs');
+// fs.writeFileSync("./it-a.text","Hello World")
+// fs.writeFileSync("./abes.text","Welcome to ABES Engineering College");
+// const res=fs.readFileSync("./it-a.text","utf-8");
+// console.log(res);
+fs.writeFile("./it-a.text","Hello World",()=>{});
